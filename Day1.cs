76c17d1b67bc0d51fc960ea3b949a67ee4cc173e
nora2605 <nora.ja2605@gmail.com>
2024-12-01 16:12:24 +0100

    initial commit

---
+++ Day1.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2024;
+public class Day1
+{
+    int[] list_left;
+    int[] list_right;
+
+    public Day1()
+    {
+        var input = new StreamReader("inputs/day1.txt")!.ReadToEnd();
+        var data = input.Split('\n').Select(l => l.Split("   ").Select(int.Parse).ToArray());
+        list_left = data.Select(x => x[0]).OrderBy(x => x).ToArray();
+        list_right = data.Select(x => x[1]).OrderBy(x => x).ToArray();
+    }
+
+    public int Day1_1() => list_left
+            .Zip(list_right)
+            .Select(x => Math.Abs(x.First - x.Second))
+            .Sum();
+
+    public int Day1_2()
+    {
+        int[] counts = new int[Math.Max(list_right.Max(), list_left.Max())+1];
+        foreach (var v in list_right) counts[v]++;
+        return list_left.Select(l => l * counts[l]).Sum();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode2024;
+
+LabelledTimer t = new();
+
+Console.WriteLine("Solutions:");
+
+// Day 1
+
+t.Start();
+Day1 d1 = new();
+t.Round("Day 1 - Init");
+
+Console.WriteLine(t.RoundWith("Day 1 - Star 1", d1.Day1_1));
+Console.WriteLine(t.RoundWith("Day 1 - Star 2", d1.Day1_2));
+
+// Day 2
+
+
+// Timing
+
+Console.WriteLine("\nTimes:");
+Console.WriteLine(t);
