e6eb20aa7dfb904d3eb03c98184a6250f22e84f9
nora2605 <nora.ja2605@gmail.com>
2024-12-10 12:11:36 +0100

    somehow this didn't change anything but it should be like wrong

--- Day8.cs
+++ Day8.cs
@@ -11,7 +11,7 @@ internal class Day8
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     ];
-    Dictionary<char, List<(int x, int y)>> antennae = [];
+    Dictionary<char, List<(int y, int x)>> antennae = [];
 
     public Day8()
     {
